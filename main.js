7f508a652aa04ebaf8bf0c7b13738aaddf67989c
KianMun <kianmun80@yahoo.com>
2023-10-17 15:00:44 +0800

    implemented time

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const PORT = parseInt(process.env.PORT) || 3000
 
 const app = express()
 
+app.get('/time', (req, resp)=>{
+    resp.send({time: (new Date()).toISOString()})
+})
+
 app.use(express.static(__dirname + '/static'))
 
 app.listen(PORT, () =>{
